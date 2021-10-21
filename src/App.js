04a907c683f783ad1f928538b6bf0e4dadf7d26e
Florencia87 <florenciafregossi@gmail.com>
2021-10-21 13:16:53 -0300

    primer commit del proyecto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sprint Project 04</h1>
+      <h2>Realizado el Git</h2>
+    </div>
+  );
+}
+
+export default App;
