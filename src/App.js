d7344cc1508467c8bb1233dbec85676e7115b61d
Florencia87 <florenciafregossi@gmail.com>
2021-10-21 14:20:29 -0300

    inicializado firebase

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Sprint Project 04</h1>
       <h2>Realizado el Git</h2>
+      <h3>Creo el repositorio y sincronizo</h3>
     </div>
   );
 }
---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBIfqu9MGVF16Aw5N0ia6-G4VfKQ3k9Cgc",
+  authDomain: "devs-united-ad8e4.firebaseapp.com",
+  projectId: "devs-united-ad8e4",
+  storageBucket: "devs-united-ad8e4.appspot.com",
+  messagingSenderId: "802114875697",
+  appId: "1:802114875697:web:484647d4638f3ded300010"
+}
+
+export const collections = {
+
+}
